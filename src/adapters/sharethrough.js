22ab983da4e2efed0f1103cf270a5c970e17446c
Peter Kinmond <peterkinmond@gmail.com>
2016-09-09 16:46:55 -0700

    test load script

--- src/adapters/sharethrough.js
+++ src/adapters/sharethrough.js
@@ -11,65 +11,70 @@ var bidfactory = require('../bidfactory.js');
         params: {
           pkey: PKEY
         }
+      },
+      {
+        bidder: "sharethrough",
+        params: {
+          foo: bar
+        }
       }
     ]
 }
 */
 
 var SharethroughAdapter = function SharethroughAdapter() {
-  const httpRequest = new XMLHttpRequest();
-  let bidId;
 
   function _callBids(params) {
     var bids = params.bids,
         scriptUrl,
-        callback = _callback,
         cacheRequest = false;
 
     // cycle through bids
     for (var i = 0; i < bids.length; i += 1) {
-      bidId = bids[i].bidId;
-      scriptUrl = _buildSharethroughCall(bids[i]);
-      httpRequest.onreadystatechange = callback;
-      httpRequest.open('GET', scriptUrl);
-      httpRequest.send();
-      //adloader.loadScript(scriptUrl, callback, cacheRequest);
+      var bidRequest = bids[i];
+      scriptUrl = _buildSharethroughCall(bidRequest);
+      adloader.loadScript(scriptUrl);//, callback, cacheRequest);
     }
   }
 
   function _buildSharethroughCall(bid) {
-    var pkey = utils.getBidIdParamater('pkey', bid.params),
-      uri = '//btlr.sharethrough.com/v4?',
-      url = document.location.protocol + uri;
-
-    url = utils.tryAppendQueryString(url, 'placement_key', pkey);
+    // var pkey = utils.getBidIdParamater('pkey', bid.params),
+    //   uri = '//btlr.sharethrough.com/v4?',
+    //   url = document.location.protocol + uri;
+    var url = "http://localhost:9292/prebid/v1?";
+    url = utils.tryAppendQueryString(url, 'bid_id', bid.bidId);
+    // url = utils.tryAppendQueryString(url, 'placement_key', pkey);
+    // url = utils.tryAppendQueryString(url, 'bidId', bidId);
+    // url = utils.tryAppendQueryString(url, 'callback', callbackName);
+    // url = utils.tryAppendQueryString(url, 'v', '$prebid.version$');
+    // url = url.slice(0, -1);
     return url;
   }
 
-  function _callback() {
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+  $$PREBID_GLOBAL$$.strcallback = function(bidResponse) {
+    var bidJson = JSON.parse(bidResponse);
+      var bidId = bidJson.creatives[0].bidId;
       let bid, bidObj = utils.getBidRequest(bidId);
-      console.log(bidObj);
-      if (httpRequest.status === 200) {
+      console.log(bidJson);
         try {
-          const response = JSON.parse(httpRequest.responseText),
-            pkey = utils.getBidIdParamater('pkey', bidObj.params),
-            windowLocation = `str_response_${bidId}`;
+          var windowLocation = `str_response_${bidId}`;
+          var pkey = utils.getBidIdParamater('pkey', bidObj.params);
+          console.log("pkey" + pkey);
           bid = bidfactory.createBid(1, bidObj);
           bid.bidderCode = 'sharethrough';
-          bid.cpm = 1.0; //response.bid
+          bid.cpm = bidJson.creatives[0].cpm; //response.bid
           const size = bidObj.sizes[0];
           bid.width = size[0];
           bid.height = size[1];
-          bid.ad = `<div data-str-native-key="${pkey}" data-stx-response-name='${windowLocation}'></div><script>var ${windowLocation} = ${httpRequest.responseText}</script><script src="//native.sharethrough.com/assets/sfp.js"></script>`;
+          console.log(bidObj);
+          console.log("pc " + bidObj.placementCode);
+          bid.ad = `<div data-str-native-key="${pkey}" data-stx-response-name='${windowLocation}'></div><script>var ${windowLocation} = ${bidResponse}</script><script src="//native.sharethrough.com/assets/sfp.js"></script>`;
           bidmanager.addBidResponse(bidObj.placementCode, bid);
+
         } catch (e) {
           _handleInvalidBid(bidObj);
         }
-      } else {
-        _handleInvalidBid(bidObj);
-      }
-    }
+
     // If the bid is valid: Use bidfactory.createBid(1) to create the bidObject.
     // If the bid is invalid (no fill or error): Use bidfactory.createBid(2) to create the bidObject.
     // bidmanager.addBidResponse(adUnitCode, bidObject);
